5acd9492a35e522932c51fa2bf1239c0d7d54f5d
lipeng <312533676@qq.com>
2017-03-14 10:58:16 +0800

    dsadasda

---
+++ app/src/main/java/come/pzwsdrz/shareutils/MainActivity.java
@@ -0,0 +1,19 @@
+package come.pzwsdrz.shareutils;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public  void share(View view){
+
+    }
+}
